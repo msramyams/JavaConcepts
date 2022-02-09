b980e0a69d6ca0b2bc12e37a3df97166d78745c8
msramyams <ramyams998@gmail.com>
2022-02-09 23:14:16 +0530

    default access modifier

---
+++ CoreJava/src/com/defaultaccprg/Default.java
@@ -0,0 +1,17 @@
+package com.defaultaccprg;
+
+public class Default 
+{
+	
+		void sample()
+		{
+			System.out.println("Default access modifier");
+			System.out.println("Can be accessed within class and within package");
+		}
+
+}
+
+
+	
+
+
---
+++ CoreJava/src/com/defaultaccprg1/Default2.java
@@ -0,0 +1,12 @@
+package com.defaultaccprg1;
+import com.defaultaccprg.*;
+public class Default2 
+{
+		public static void main(String[] args) 
+		{
+			Default d=new Default();// gives compile time error
+			//d.sample();  //gives compile time error in import pack.*
+
+		}
+
+	}
