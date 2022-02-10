3ad32bb22b8098562128b13882e85bbf0c3b55aa
msramyams <ramyams998@gmail.com>
2022-02-10 21:58:26 +0530

    Hello program using bufferedreader

---
+++ CoreJava/src/com/bufferedprgs/Hello.java
@@ -0,0 +1,16 @@
+package com.bufferedprgs;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Hello
+{
+	public static void main(String[] args) throws Exception
+	{
+	System.out.println("Enter string: ");
+	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	String str=br.readLine();
+	
+	}
+	
+
+}
