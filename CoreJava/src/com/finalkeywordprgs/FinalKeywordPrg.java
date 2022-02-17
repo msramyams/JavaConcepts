a271e9f7773ba4a4dde56f417339163f2ffd82c9
msramyams <ramyams998@gmail.com>
2022-02-17 21:58:21 +0530

    final keyword for class,variable,methods

---
+++ CoreJava/src/com/finalkeywordprgs/FinalKeywordPrg.java
@@ -0,0 +1,37 @@
+package com.finalkeywordprgs;
+
+	//final keyword program which is being used for class, method, variable
+		final class Student1
+		{
+			
+			final void id()
+			{
+				System.out.println("student id");
+				
+			}
+		}
+		//class Student2 extends Student1 // if i declare super class as final it cannot be inherited 
+		//{
+			//void id() // if i declare method as final it cannot override
+//		{
+				
+//		}
+		//}
+
+		public class FinalKeywordPrg 
+		{
+
+			public static void main(String[] args)
+			{
+				final int a=40;
+			//	a++;// cannot be changed after declaring variable as final
+				Student1 s=new Student1();
+				s.id();
+				System.out.println(a);
+				
+				
+
+			}
+
+		}
+
