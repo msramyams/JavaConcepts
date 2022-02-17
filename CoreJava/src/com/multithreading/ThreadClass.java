de417fd7b3db2abad2dd95f80174b02ffa4983e3
msramyams <ramyams998@gmail.com>
2022-02-17 20:14:42 +0530

    By implementing Runnable Thread

---
+++ CoreJava/src/com/multithreading/ThreadClass.java
@@ -0,0 +1,10 @@
+package com.multithreading;
+//by implementing Runnable interface
+public class ThreadClass implements Runnable 
+{
+	public void run()
+	{
+		System.out.println("Hello from a thread");
+	}
+
+}
