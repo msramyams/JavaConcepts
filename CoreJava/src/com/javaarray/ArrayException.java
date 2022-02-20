762bcc62d459a2ba2dd7d4a9a7f8947906522131
msramyams <ramyams998@gmail.com>
2022-02-21 00:03:38 +0530

    program for array exception

---
+++ CoreJava/src/com/javaarray/ArrayException.java
@@ -0,0 +1,17 @@
+package com.javaarray;
+//ArrayIndexOutOfBoundsException in a Java Array.  
+public class ArrayException 
+{
+
+
+	public static void main(String args[])
+	{  
+	int arr[]={50,60,70,80,90};  
+	for(int i=0;i<=arr.length;i++)
+		{  
+	System.out.println(arr[i]);  
+		}  
+	}
+}  
+
+
