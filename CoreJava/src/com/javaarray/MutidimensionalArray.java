64e8b64258d7cd1a0a895aeb188624ffcc8d3e54
msramyams <ramyams998@gmail.com>
2022-02-21 00:04:53 +0530

    java array for multidimension

---
+++ CoreJava/src/com/javaarray/MutidimensionalArray.java
@@ -0,0 +1,23 @@
+package com.javaarray;
+//Java Program  using multidimensional array
+public class MutidimensionalArray 
+{
+	  
+	
+	public static void main(String args[])
+	{  
+	
+	int arr[][]={{1,2,3},{2,4,5},{4,4,5}};  //declaring and initializing 2D array  
+	
+	for(int i=0;i<3;i++)	//row
+	{  
+	 for(int j=0;j<3;j++)	//column
+	 {  
+	   System.out.print(arr[i][j]+" ");  
+	 }  
+	 System.out.println();  
+	}  
+	}
+	}  
+
+
